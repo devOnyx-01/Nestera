0194473dd8c0625c9e7b387c508b3490f13dfdf3
alexohre <alexokomado@gmail.com>
2026-03-30 08:40:34 +0100

    feat(admin): register transaction monitoring components in AdminModule

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -10,30 +10,37 @@ import { AdminWaitlistController } from './admin-waitlist.controller';
 import { AdminUsersController } from './admin-users.controller';
 import { AdminUsersService } from './admin-users.service';
 import { AdminSavingsService } from './admin-savings.service';
+import { AdminTransactionsController } from './admin-transactions.controller';
+import { AdminTransactionsService } from './admin-transactions.service';
+import { AdminTransactionNote } from './entities/admin-transaction-note.entity';
 import { User } from '../user/entities/user.entity';
 import { UserSubscription } from '../savings/entities/user-subscription.entity';
 import { SavingsProduct } from '../savings/entities/savings-product.entity';
 import { LedgerTransaction } from '../blockchain/entities/transaction.entity';
+import { Transaction } from '../transactions/entities/transaction.entity';
 
 @Module({
   imports: [
+    UserModule,
+    SavingsModule,
+    MailModule,
+    BlockchainModule,
     TypeOrmModule.forFeature([
       User,
       UserSubscription,
       SavingsProduct,
       LedgerTransaction,
+      Transaction,
+      AdminTransactionNote,
     ]),
-    UserModule,
-    SavingsModule,
-    MailModule,
-    BlockchainModule,
   ],
   controllers: [
     AdminController,
     AdminSavingsController,
     AdminWaitlistController,
     AdminUsersController,
+    AdminTransactionsController,
   ],
-  providers: [AdminUsersService, AdminSavingsService],
+  providers: [AdminUsersService, AdminSavingsService, AdminTransactionsService],
 })
 export class AdminModule {}
